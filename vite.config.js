889da0ca78015afbb5a64980dd526db9eb385c25
llddang <bodyness@naver.com>
2025-02-27 10:54:55 +0900

    feat:#1 절대 경로 설정

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,17 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()]
+  plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: [
+      { find: '@', replacement: '/src' },
+      { find: '@ui', replacement: '/src/components/ui' },
+      { find: '@features', replacement: '/src/components/features' },
+      { find: '@layouts', replacement: '/src/components/layouts' },
+      { find: '@pages', replacement: '/src/pages' },
+      { find: '@hooks', replacement: '/src/lib/hooks' },
+      { find: '@api', replacement: '/src/lib/apis' },
+      { find: '@utils', replacement: '/src/lib/utils' }
+    ]
+  }
 });
