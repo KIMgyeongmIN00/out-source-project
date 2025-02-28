2493b5f596a0fb62cbbacdea53859f72fd444f0f
llddang <bodyness@naver.com>
2025-02-28 15:39:51 +0900

    design:#17 사이드바에 유저 정보 패널 추가

---
+++ src/components/layouts/sidebar/user-panel.jsx
@@ -0,0 +1,29 @@
+import { Link } from 'react-router-dom';
+import { MdOutlinePerson } from 'react-icons/md';
+import { MdLogout } from 'react-icons/md';
+import { Button } from '@/components/ui/button';
+
+export default function UserPanel({ user }) {
+  return (
+    <session className="flex flex-col gap-4 items-center">
+      <img
+        src={user.profileUrl || '/default_profile.png'}
+        alt={`${user.nickname} 프로필`}
+        className="w-30 rounded-full border-2 border-primary aspect-square object-cover object-top"
+      />
+      <p className="text-xl">
+        <b className="text-primary">{user.nickname}</b> 님
+      </p>
+      <div className="space-x-4">
+        <Button size="icon">
+          <Link to="/profile">
+            <MdOutlinePerson />
+          </Link>
+        </Button>
+        <Button size="icon">
+          <MdLogout />
+        </Button>
+      </div>
+    </session>
+  );
+}
