b4ab0fe3e9b0c731775ef94cc22c02204b5e1add
sharet9446 <spees97@naver.com>
2025-03-01 01:28:49 +0900

    feat: 카카오 맵 REST KEY 추가 및 axios 인스턴스 설정

--- src/lib/apis/map.api.js
+++ src/lib/apis/map.api.js
@@ -1,5 +1,7 @@
 import { KAKAO_MAP_API_KEY } from '@/constants/app-key';
 import { useKakaoLoader } from 'react-kakao-maps-sdk';
+import { useQuery } from '@tanstack/react-query';
+import { axiosMap } from './axios.api';
 
 export function useKakaoMapQuery() {
   const [loading, error] = useKakaoLoader({
@@ -8,3 +10,18 @@ export function useKakaoMapQuery() {
 
   return { kakaoMapLoading: loading, kakaoMapError: error };
 }
+
+export const useKakaoAddressQuery = (lat, lng) => {
+  return useQuery({
+    queryKey: ['kakaoAddress', lat, lng],
+    queryFn: async () => {
+      try {
+        const response = await axiosMap.get(`coord2address.json?x=${lng}&y=${lat}`);
+        return response.data;
+      } catch (error) {
+        console.error('Registration error:', error);
+        throw error;
+      }
+    }
+  });
+};
