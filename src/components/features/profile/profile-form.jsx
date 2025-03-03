47dbb3e89ef53ab4d66bd5a9380ff0c220d14d15
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-03 21:48:06 +0900

    feat:#36 프로필 업로드 기능 추가

--- src/components/features/profile/profile-form.jsx
+++ src/components/features/profile/profile-form.jsx
@@ -3,14 +3,29 @@ import { Input } from '@/components/ui/input';
 import { useState } from 'react';
 import { useAuthStore } from '@/stores/auth.store';
 import useUpdateProfileMutation from '@/lib/hooks/use-update-profile-mutation';
+import useUploadImageMutation from '@/lib/hooks/use-upload-image-mutation';
 
 export default function ProfileForm() {
   const [editMode, setEditMode] = useState(false);
   const [nickname, setNickname] = useState('');
-
+  const [image, setImage] = useState({});
   const user = useAuthStore((state) => state.user);
 
   const { mutate: updateProfile } = useUpdateProfileMutation();
+  const { mutate: uploadProfileImage } = useUploadImageMutation(image, user);
+
+  function handleImageChange(e) {
+    const file = e.target.files[0];
+    setImage(file);
+  }
+
+  function uploadFile() {
+    uploadProfileImage(image);
+  }
+
+  function cancelUpdate() {
+    setEditMode((prev) => !prev);
+  }
 
   const handleSubmit = (e) => {
     e.preventDefault();
@@ -33,17 +48,32 @@ export default function ProfileForm() {
   return (
     <form onSubmit={handleSubmit} className="flex flex-row justify-between items-center w-full gap-4">
       <div className="flex flex-row items-center gap-4 ml-5">
-        <img src="/public/default_profile.png" alt="프로필 이미지" className="w-20 h-20 rounded-full" />
+        <img src={user.profileUrl} alt="프로필 이미지" className="w-30 h-20 rounded-full" />
         <div>
           <p>반갑습니다</p>
           {editMode ? (
-            <Input value={nickname} onChange={(e) => setNickname(e.target.value)} />
+            <>
+              <div className="flex flex-row items-center gap-2">
+                <Input type="file" onChange={handleImageChange} required />
+                <Button type="button" onClick={uploadFile}>
+                  이미지 업로드
+                </Button>
+              </div>
+              <Input value={nickname} onChange={(e) => setNickname(e.target.value)} required />
+            </>
           ) : (
             <p>{`${user.nickname} 님`}</p>
           )}
         </div>
       </div>
-      {editMode ? <Button>수정완료</Button> : <Button>프로필 수정</Button>}
+      {editMode ? (
+        <div className="flex gap-2">
+          <Button onClick={cancelUpdate}>수정 취소</Button>
+          <Button>수정 완료</Button>
+        </div>
+      ) : (
+        <Button>프로필 수정</Button>
+      )}
     </form>
   );
 }
