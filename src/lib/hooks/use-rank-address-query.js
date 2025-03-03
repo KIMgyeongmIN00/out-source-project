9107bacd9f9ae66e858e310f518bedbc6384168d
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-03 15:48:28 +0900

    feature:#31 plans가 아예 없는 신규 회원을 위한 예외처리

--- src/lib/hooks/use-rank-address-query.js
+++ src/lib/hooks/use-rank-address-query.js
@@ -4,6 +4,9 @@ export default function useRankAddressUseQuery(plans) {
   const { data: topLocations = [] } = useQuery({
     queryKey: ['topLocations', plans],
     queryFn: () => {
+      if (!plans || plans.length === 0) {
+        return [];
+      }
       const countByPlan = plans.reduce((acc, plan) => {
         acc[plan.address] = (acc[plan.address] || 0) + 1;
         return acc;
