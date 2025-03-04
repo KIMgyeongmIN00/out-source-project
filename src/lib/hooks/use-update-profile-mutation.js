a547b6cb8b2973ddadf0cf07d5a2e8c231a32788
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-04 15:13:27 +0900

    refactor:#36 프로필 업데이트 기능 리팩토링

--- src/lib/hooks/use-update-profile-mutation.js
+++ src/lib/hooks/use-update-profile-mutation.js
@@ -7,32 +7,29 @@ export default function useUpdateProfileMutation() {
 
   return useMutation({
     mutationFn: async ({ image, userId, nickname }) => {
-      try {
-        const { error: updateError } = await supabase.from('users').update({ nickname }).eq('id', userId);
-
-        if (updateError) {
-          sweetAlert.error('닉네임 업데이트 중 오류가 발생했습니다.');
-          return;
-        }
-
-        await supabase.storage.from('images').upload(`/profile/${image.name}`, image);
-
-        const profileImageURL = supabase.storage.from('images').getPublicUrl(`profile/${image.name}`).data.publicUrl;
-
-        const { error: updateUrlError } = await supabase
-          .from('users')
-          .update({ profile_url: profileImageURL })
-          .eq('id', userId);
-
-        if (updateUrlError) {
-          sweetAlert.error('프로필 이미지 URL 업데이트 중 오류가 발생했습니다.');
-          return;
-        }
-        sweetAlert.success('프로필 수정 완료');
-      } catch (error) {
-        sweetAlert.error('프로필 수정 중 오류가 발생했습니다.');
-        console.error(error);
+      const { error: imageUploadError } = await supabase.storage.from('images').upload(`/profile/${image.name}`, image);
+      if (imageUploadError) {
+        sweetAlert.error('프로필 이미지 업로드 중 오류가 발생했습니다.');
+        return;
       }
+
+      const profileImageURL = supabase.storage.from('images').getPublicUrl(`profile/${image.name}`).data.publicUrl;
+
+      const { error: updateImageUrlError } = await supabase
+        .from('users')
+        .update({ profile_url: profileImageURL })
+        .eq('id', userId);
+      if (updateImageUrlError) {
+        sweetAlert.error('프로필 이미지 URL업데이트 중 오류가 발생했습니다.');
+      }
+
+      const { error: updateNicknameError } = await supabase.from('users').update({ nickname }).eq('id', userId);
+      if (updateNicknameError) {
+        sweetAlert.error('프로필 수정 중 오류가 발생했습니다.');
+        return;
+      }
+
+      sweetAlert.success('프로필 수정 완료');
     },
     onSuccess: () => queryClient.invalidateQueries({ queryKey: ['users'] })
   });
