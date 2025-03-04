073d08263866e9e125034c949e7f1d49de2d96bd
Jongyeon Kim <tyjysong0712@gmail.com>
2025-03-04 12:43:17 +0900

    style:#36 프로필 이미지에 object-cover 적용

--- src/components/features/profile/profile-form.jsx
+++ src/components/features/profile/profile-form.jsx
@@ -51,7 +51,11 @@ export default function ProfileForm() {
   return (
     <form onSubmit={handleSubmit} className="flex flex-row justify-between items-center w-full gap-4">
       <div className="flex flex-row items-center gap-4 ml-5">
-        <img src={user.profileUrl} alt="프로필 이미지" className="w-20 h-20 border-2 border-primary rounded-full" />
+        <img
+          src={user.profileUrl}
+          alt="프로필 이미지"
+          className="w-20 h-20 border-2 object-cover border-primary rounded-full"
+        />
         <div>
           <p>반갑습니다</p>
           {editMode ? (
