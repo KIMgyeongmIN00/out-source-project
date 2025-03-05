8ec8a35695d9d1c1d5227431a75867eee5d22978
llddang <bodyness@naver.com>
2025-03-05 10:36:05 +0900

    feat: #50 sidebar의 로고 커서 변경

--- src/components/layouts/sidebar/sidebar.jsx
+++ src/components/layouts/sidebar/sidebar.jsx
@@ -1,17 +1,18 @@
-import { useNavigate } from 'react-router-dom';
+import { Suspense } from 'react';
+import { Link } from 'react-router-dom';
 import PlanPanel from '@/components/layouts/sidebar/plan-panel';
 import SignPanel from '@/components/layouts/sidebar/sign-panel';
 import UserPanel from '@/components/layouts/sidebar/user-panel';
 import { useAuthStore } from '@/stores/auth.store';
-import { Suspense } from 'react';
 
 export default function Sidebar() {
   const { isAuthenticated: isAuth, user } = useAuthStore();
-  const navigate = useNavigate();
 
   return (
     <div className="w-[250px] h-full overflow-hidden flex flex-col gap-8 border border-primary rounded-2xl px-4 pt-8 pb-6">
-      <img src="/logo.png" alt="로고 이미지" className=" w-full box-border px-6" onClick={() => navigate('/')} />
+      <Link to="/">
+        <img src="/logo.png" alt="로고 이미지" className="w-full box-border px-6 cursor-pointer" />
+      </Link>
       {isAuth ? <UserPanel user={user} /> : <SignPanel />}
       <Suspense fallback={<div>Loading plans...</div>}>{isAuth && <PlanPanel userId={user.id} />}</Suspense>
     </div>
